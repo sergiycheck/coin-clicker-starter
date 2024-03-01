bd3005fdb72b9797bc21215ea63b708e33aff9f6
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-03-01 17:49:50 +0200

    enabled eruda

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import eruda from "eruda";
+import eruda from "eruda";
 
-// eruda.init();
+eruda.init();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
