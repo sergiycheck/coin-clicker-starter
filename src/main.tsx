01c02bf6e9de2c4b3f9533a767bba891b15f8c30
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-03-01 14:24:42 +0200

    feat: add eruda to debug

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import eruda from "eruda";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+eruda.init();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
